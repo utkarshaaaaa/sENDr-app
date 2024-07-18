c685c0cd8c39154115aaee520912f8bd3e739fb7
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-07-18 18:35:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
     }
    
   ])
-  const a=2
+  const a=1
   return (
     <div className="App">
 
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleLogin } from "@react-oauth/google";
-import { googleLogout } from '@react-oauth/google';
-
+import { googleLogout } from "@react-oauth/google";
+import jwt_decode, { jwtDecode } from 'jwt-decode'
 
 export default function () {
+  const[data,setdata]=useState("")
+
+  const getSignUpData=()=>{
+    const decodedData=jwtDecode(data)
+    console.log(decodedData)
+    setdata(decodedData)
+
+
+  }
   return (
     <div>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
           console.log(credentialResponse);
+          setdata(credentialResponse.credential)
         }}
         onError={() => {
           console.log("Login Failed");
         }}
       />
-      ;
+      <button onClick={()=>{getSignUpData()}}>Get the dataa</button>
+      {data.email}
+
+      <img src={data.picture}/>
+      
     </div>
   );
 }
