4d8877fdc585ef4792efc015ba50bae22cc2522b
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-07-29 15:31:03 +0530

    commit

--- src/Pages/SideBar.js
+++ src/Pages/SideBar.js
@@ -3,21 +3,32 @@ import React from "react";
 import "../Design/sidebar.css";
 
 export default function SideBar() {
-
   return (
     <>
+      <div className="nav-symbol-container">
+        <div className="semi-circle"></div>
+        <div className="nav-symbol">☰</div>
+      </div>
       <div className="nav-symbol">☰</div>
-      <div className="navbar">
-         
-      <a href='/'className="nav-item">Home</a>
-        
-          <a href='/followers'className="nav-item">Followers</a>
-       
 
-        <a className="nav-item" href='/'>Posts</a>
-        <a className="nav-item" href='/'>Saved</a>
-        <a  href='/UserProfile'className="nav-item">Profile</a>
-       
+      <div className="navbar">
+        <a href="/" className="nav-item">
+          Home
+        </a>
+
+        <a href="/followers" className="nav-item">
+          Followers
+        </a>
+
+        <a className="nav-item" href="/">
+          Posts
+        </a>
+        <a className="nav-item" href="/">
+          Saved
+        </a>
+        <a href="/UserProfile" className="nav-item">
+          Profile
+        </a>
       </div>
     </>
   );
