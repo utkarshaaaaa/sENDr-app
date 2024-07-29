001793296c8483fa1370712d4f7edb458e3cdba9
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-07-29 15:54:21 +0530

    commit

--- src/Pages/SideBar.js
+++ src/Pages/SideBar.js
@@ -1,16 +1,37 @@
 import React from "react";
-
+import { useEffect, useState, useContext } from "react";
 import "../Design/sidebar.css";
 
 export default function SideBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuHovered, setMenuHovered] = useState(false);
+
+
+
+  const handleMouseEnter = () => {
+    setMenuHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMenuHovered(false);
+  };
+
+    
+  
   return (
     <>
-      <div className="nav-symbol-container">
-        <div className="semi-circle"></div>
-        <div className="nav-symbol">☰</div>
-      </div>
-      <div className="nav-symbol">☰</div>
+      {/* <div className="nav-symbol">☰</div> */}
 
+      <div
+        className={`container-sidebar ${menuHovered ? 'change' : ''}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+    
       <div className="navbar">
         <a href="/" className="nav-item">
           Home
