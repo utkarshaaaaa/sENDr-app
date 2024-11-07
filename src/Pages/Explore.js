fe59ff08f7c676b62ac54b477c2eaf0e5ba526d0
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-11-07 22:06:39 +0530

    commit

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -37,6 +37,18 @@ export default function Explore() {
                     <li>Followers:{userData?.Followers?.length}   </li>
                     <li>Following:{userData?.Following?.length}</li>
                 </ul>
+
+               {/* {
+                let arr = [
+                  { name:"string 1", value:"this", other: "that" },
+                  { name:"string 2", value:"this", other: "that" }
+              ];
+              
+              let obj = arr.find(o => o.name === 'string 1');
+              
+              console.log(obj);
+              
+               } */}
                
                 <button class="card__button">Follow</button>
                 
